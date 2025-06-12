8d75e391df5401e589b9133f2855288fa9551770
victor-mungai <mungaivictor2781@gmail.com>
2025-06-12 16:57:19 +0400

    set up s3 using python boto3

---
+++ s3setup.py/s3test.py
@@ -0,0 +1,18 @@
+import boto3
+#create an s3 client
+
+s3 = boto3.client('s3') 
+
+# define the bucket name and the file to upload
+
+bucket_name = 'botopython27811234'
+file_name = input("Enter the path of the file to upload: ")
+object_name = file_name
+
+#upload file to s3
+try:
+    
+    s3.upload_file(file_name, bucket_name, object_name)
+    print(f"File {file_name} uploaded to bucket {bucket_name} as {object_name}.")
+except Exception as e:
+    print(f"Error uploading file: {e}")
